08fb7dddd84a03942a40df23b92b0230beee618b
Your Name <you@example.com>
2024-08-13 14:16:50 +0000

    add a main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int read_one_by_one()
+int readline(void)
+int print_parent_pid(void)
+
+
+
+
+#endif
