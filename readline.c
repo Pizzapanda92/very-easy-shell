230e00eacf109a1e98a64a642db4212fb02ace68
Your Name <you@example.com>
2024-08-13 10:15:21 +0000

    add readline test

---
+++ readline.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while (1) 
+    {
+        printf("$ ");
+
+        read = getline(&line, &len, stdin);
+        if (read == -1) 
+	{
+            break;
+        }
+
+        printf("Vous avez tapé : %s", line);
+    }
+
+    free(line);
+    exit(EXIT_SUCCESS);
+}
