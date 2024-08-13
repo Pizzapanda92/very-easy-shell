93f51eb80a913b6708f57c97b98aef361bd0b7a9
Your Name <you@example.com>
2024-08-13 21:50:07 +0000

    add fork_test pour lexo Exercise: fork + wait + execve

---
+++ fork_test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int create_child_and_execute()
+{
+    int i;
+    pid_t pid_fils;
+    char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
+    extern char **environ; 
+
+    for (i = 0; i < 5; i++)
+    {
+        pid_fils = fork();
+
+        if (pid_fils == -1)
+        {
+            perror("Erreur lors de la création de l'enfant");
+            return (1);
+        }
+
+        if (pid_fils == 0)
+        {
+            execve(argv[0], argv, environ);
+            perror("Erreur lors de l'exécutin de execve");
+            return 1;
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+    return (0);
+}
+
+int main(void)
+{
+    return create_child_and_execute();
+}
+
