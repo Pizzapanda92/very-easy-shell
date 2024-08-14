7f6ccc2de3d5f278d3882d8fa37b54302e7c89d7
bluelightwatcher <9550@holbertonstudents.com>
2024-08-14 10:13:49 +0200

    Program executes a binary (the ls cmd)

--- execv.c
+++ execv.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    char *argv[] = {"/bin/bash", "ls", NULL}; // Arguments for the interpreter
+    char *argv[] = {"/usr/bin/ls", NULL}; // Arguments for the interpreter
     char *envp[] = {NULL}; // Use current environment
 
-    if (execve("/bin/bash", argv, envp) == -1) {
+    if (execve("/usr/bin/ls", argv, envp) == -1) {
         perror("execve failed"); // Print error if execve fails
         return 1;
     }
